002647ed10e15f4ccabc46545052ef30d62f9ca2
Wildan Ziaulhaq <wildan2wildan@gmail.com>
2018-02-11 21:22:39 +0700

    Fix mail type

--- application/controllers/mail_type.php
+++ application/controllers/mail_type.php
@@ -6,7 +6,7 @@ class Mail_type extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		if ($this->session->userdata('level') != 1) {
+		if (!$this->session->userdata('login')) {
 			redirect('/');
 		}
 		$this->load->model('mail_type_model');
